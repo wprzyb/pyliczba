c69d0cc30e9f23432f328dcb077ab49de7d00ae5
Wiktor Przybylski <wikop1@gmail.com>
2015-04-23 20:51:01 +0200

    Made setup.py file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+ 
+setup(
+    name = 'pyliczba',
+    packages = ['pyliczba'],
+    version = '0.0.1',
+    description = 'Convert numbers to words in polish',
+    author='SQ9WTF', # You will change that
+    author_email='and@that.as.well', # <---
+    url='https://github.com/sq6jnx/pyliczba',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Topic :: Software Development :: Localization'
+    ]
+)
